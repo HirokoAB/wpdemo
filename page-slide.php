262a9901f549083dc6c03e29f20bbc72e33870de
阿部寛子 <hihoyeahi@gmail.com>
2019-06-17 08:28:10 +0700

    get_post_meta 記述の保存

--- page-slide.php
+++ page-slide.php
@@ -13,22 +13,12 @@
 <?php get_footer(); ?>
 
 
-          <?php $meta = get_post_meta( get_the_ID() ); ?>
-          <?php var_dump( $meta ); ?>
 
 <?php 
-if ( !function_exists( 'get_custum_post_types' ) ):
-function get_custum_post_types(){
-  $args = array(
-    'public' => true,
-    '_builtin' => false
-  );
-  $post_types = get_post_types( $args );
-  return $post_types;
-}
-endif;
-
-?>
+$value = get_post_meta( get_the_ID(), 'slider1' , true );
+if ( $value ){
+  echo $value;
+} ?>
 
 
 
