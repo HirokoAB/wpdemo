885e9326401ef150cff5ca44cfdfed0e5543c861
阿部寛子 <hihoyeahi@gmail.com>
2019-06-17 09:53:07 +0700

    slideの値取得まで成功　しかしこれでは...

--- page-slide.php
+++ page-slide.php
@@ -1,18 +1,5 @@
-<?php get_header(); ?>
-  
 
-  <main class="articles">
-  <div id="blog_archive">
-    <h1>最近の記事一覧</h1>
-    <?php if (have_posts()): ?>
-      <?php foreach ( $posts as $post ): ?>
-<!--           <?php the_meta( 'slider1' ); ?> -->
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </div>
-<?php get_footer(); ?>
 
- 
           <?php $meta = get_post_meta( get_the_ID() ); ?>
           <?php var_dump( $meta ); ?>
 <?php 
@@ -21,6 +8,74 @@ $value = get_post_meta( get_the_ID(), 'slider1' , true )
 <img src="<?php echo ( $value ); ?>">
 
 <?php var_dump ( $value );   ?>
+
+
+
+
+
+<?php
+$postid = $post -> ID;
+ 
+    $args = array(
+    'numberposts' => -1,//取得件数（初期値は5件、-1で全添付を取得）
+  'order' => 'ASC',//並び順
+  'orderby' => 'menu_order',//並び順の規準
+        'post_type' => 'attachment',//取得種類（この指定で添付を取得できる）
+        'post_mime_type' => 'image',//取得種類（この指定で添付の種類ギャラリーを取得できる）
+        //'post_mime_type' => 'application/pdf',//PDFファイル
+        //'post_mime_type' => 'audio',//audioファイル
+        //'post_mime_type' => 'video',//videoファイル
+        //'post_mime_type' => 'application/msword',//wordファイル
+        //'post_mime_type' => 'application/doc',//wordファイル
+        'post_parent' => $postid//帰属する投稿ID
+    );
+    $attachments = get_posts( $args );
+ 
+    if ( $attachments ) {
+        foreach ( $attachments as $attachment ) {
+ 
+            $imgtag = wp_get_attachment_image( $attachment->ID, 'thumbnail' );//thumbnail, medium, large, full
+            $img = wp_get_attachment_image_src( $attachment->ID, 'thumbnail' );//thumbnail, medium, large, full
+            $imgURL = $img[0];//取得したイメージのURL
+            $imgWidth = $img[1];//取得したイメージの幅
+            $imgHeight = $img[2];//取得したイメージの高さ
+ 
+            $ID = $attachment->ID;//添付ID
+            $post_author = $attachment->post_author;//投稿者ID
+            $post_content = $attachment->post_content;//「説明」
+            $post_title = $attachment->post_title;//「タイトル」初期値はファイル名
+            $post_excerpt = $attachment->post_excerpt;//「キャプション」
+            $post_parent = $attachment->post_parent;//帰属する投稿ID
+            $guid = $attachment->guid;//オリジナル画像のURL
+ 
+        }
+    }
+
+    var_dump($guid);
+    var_dump($imgURL);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
