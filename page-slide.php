f4a949007930f11e67a7180af7f358c8b6e75f3c
阿部寛子 <hihoyeahi@gmail.com>
2019-06-17 09:19:33 +0700

    imgタグでの出力まで

--- page-slide.php
+++ page-slide.php
@@ -6,21 +6,37 @@
     <h1>最近の記事一覧</h1>
     <?php if (have_posts()): ?>
       <?php foreach ( $posts as $post ): ?>
-          <?php the_meta( 'slider1' ); ?>
+<!--           <?php the_meta( 'slider1' ); ?> -->
         <?php endforeach; ?>
       <?php endif; ?>
     </div>
 <?php get_footer(); ?>
 
-
-
+ 
+          <?php $meta = get_post_meta( get_the_ID() ); ?>
+          <?php var_dump( $meta ); ?>
 <?php 
-$value = get_post_meta( get_the_ID(), 'slider1' , true );
-if ( $value ){
+$value = get_post_meta( get_the_ID(), 'slider1' , true )
+;?>
+<img src="<?php echo ( $value ); ?>">
+
+<?php var_dump ( $value );   ?>
+
+
+
+
+
+
+
+
+
+
+
+<!-- if ( $value ){
   echo $value;
 } ?>
 
-
+ -->
 
 
 
