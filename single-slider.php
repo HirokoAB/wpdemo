3f7bcb0b10ddc21999ad4b02147facd924103313
阿部寛子 <hihoyeahi@gmail.com>
2019-06-18 11:01:07 +0700

    function触る前のバックアップ

--- single-slider.php
+++ single-slider.php
@@ -19,8 +19,25 @@
     <div><img class="item"src="<?php echo wp_get_attachment_url(get_post_meta($post->ID,"slider2",true));?>"></div>
     <div><img class="item"src="<?php echo wp_get_attachment_url(get_post_meta($post->ID,"slider3",true));?>"></div>
   </div>
-  <?php $test = CFS()->get( 'slide' );  ?>
-  <?php var_dump( $test) ?>
+
+
+	  <div class="owl-carousel owl-theme">
+    <div><img class="item"src="<?php echo CFS()->get( 'slider1' ); ?>" ></div>
+    <div><img class="item"src="<?php echo CFS()->get( 'slider2' ); ?>"></div>
+    <div><img class="item"src="<?php echo CFS()->get( 'slider3' ); ?>"></div>
+  	</div>
+
+  	   <?php $test = CFS()->get( 'slider1' );  ?>
+  		<?php var_dump( $test) ?>
+
+
+
+
+
+
+
+
+
 
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/owl/assets/js/jquery.min.js"></script><!--  jQuery 1.7以上  -->
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/owl/assets/js/owl.carousel.js"></script>
