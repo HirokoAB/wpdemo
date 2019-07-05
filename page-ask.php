ba9a9dff378c23ef7ef29ff77e76fa8e42a1cfdd
阿部寛子 <hihoyeahi@gmail.com>
2019-07-05 09:34:43 +0700

    page-askの空白行の削除

--- page-ask.php
+++ page-ask.php
@@ -1,17 +1,9 @@
 <?php get_header();  ?>
+<div class="container">
+	<div class="row">
 <?php echo do_shortcode('[contact-form-7 id="7" title="Contact form 1"]'); ?>
-
-
-
-
-
-
-
-
-
-
-
-
+	</div>
+</div>
 <?php 
 	//echo <<<EOM
 // 	<script>
