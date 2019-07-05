8e0822927901b1ad7839db37f5cbf7c71f6dc77e
阿部寛子 <hihoyeahi@gmail.com>
2019-07-05 16:24:38 +0700

    search画面実装ならず

--- search.php
+++ search.php
@@ -28,64 +28,8 @@ $search = new WP_Query($search_query);
 <?php //echo get_search_query(); ?>
 
 
-<h1><?php the_search_query(); ?> の検索結果</h1>
- 
-        <?php
-        if (have_posts() && get_search_query()) : 
-            while (have_posts()) :
-            the_post();
-            get_template_part( 'template-parts/post/content', 'excerpt' );
-        endwhile;
-        ?>
- 
-        <?php else : ?>
-            <div class="col-full">
-                <div class="wrap-col">
-                    <p>検索キーワードに該当する記事がありませんでした。</p>
-                </div>
-            </div>
-        <?php endif; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php if(have_posts() ):?>
 <?php while(have_posts()): the_post(); ?> 
-			<div class="">	
-				<?php  the_post_thumbnail()?>
-			</div>
 			<div class="title">	
 				<h1><?php the_title(); ?></h1>
 			</div>
@@ -107,5 +51,10 @@ $search = new WP_Query($search_query);
 		</div>
 	</div>
 </div>
+
+    <?php echo '<div>'.paginate_links(array(
+      'total' => $loop -> max_num_pages
+    )); ?>
+
 </main>
 <?php get_footer(); ?>
