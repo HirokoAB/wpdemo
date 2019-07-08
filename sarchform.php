4a683e683a7306b87bbf553e92a83b56266cf880
阿部寛子 <hihoyeahi@gmail.com>
2019-07-08 14:54:02 +0700

    singl-info htmlの修正

---
+++ sarchform.php
@@ -0,0 +1,10 @@
+ 
+<div class="search">
+<form id="cse-search-box" action="http://google.com/cse">
+<input type="hidden" name="cx" value="006360967941396448965:9e-xvz0kwly" />
+<input type="hidden" name="ie" value="UTF-8" />
+<input type="text" name="q" size="31" class="form" />
+<input type="submit" name="sa" value=" 検索" class="sc_submit" />
+</form>
+<script type ="text/javascript"src="http://www.google.com/cse/images/google_custom_search_smwide.gif"></script>
+</div>
