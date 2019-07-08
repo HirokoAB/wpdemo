d1c188191eaf13a716e9cdc0fe9115ed17b2f3af
阿部寛子 <hihoyeahi@gmail.com>
2019-07-08 11:45:53 +0700

    sarchform 完成

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,9 @@
 
 <!-- もしファンクションに'dynamic_sidebar'があれば -->
 <div class="sidebar ">
-
+<?php get_template_part('sarchform'); ?>
 <?php dynamic_sidebar('サイドバー1'); ?>
 <?php echo do_shortcode('[yop_poll id=1]'); ?>
 <?php echo do_shortcode('[TheChamp-FB-Comments]'); ?>
 <?php echo do_shortcode('[TheChamp-Sharing type="vertical"]'); ?>
-
-
 </div>
